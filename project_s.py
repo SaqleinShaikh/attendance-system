eeb6e338c2b6453f9f0a8d670ad85f2e19a4efee
saqlein shaikh <60664102+SaqleinShaikh@users.noreply.github.com>
2020-08-06 22:15:30 +0530

    Update project_s.py
    New system specification added with webcam detection and html template email .

--- project_s.py
+++ project_s.py
@@ -1,32 +1,14 @@
-from datetime import datetime
-import array as arr
-from getpass import getpass
-import struct 
 from tkinter import *
 import tkinter.messagebox 
-import smtplib
-import pandas as pd 
-import matplotlib.pyplot as plt 
-
-class person(object ):
-   def __init__(self, id_new, name,mob_n,passwd,mail,cityy):
-      self.id_new=id_new 
-      self.name = name
-      self.mob_n = mob_n
-      self.passwd = passwd 
-      self.mail=mail
-      self.cityy=cityy
-
-class personp(object ):
-   def __init__(self,ch):
-      self.ch=ch
-
+import cv2 ,time,datetime
+import e,capture
+import mysql.connector
 
 
 ################################INTERFACE###################################
 root=Tk()
 root.title("Employee ATTENDANCE system")
-root.geometry('1350x650+0+0')
+root.geometry('1550x850+0+0')
 root.configure(background="POWDER BLUE")
 
 Tops=Frame(root,width=1350,height=50,bd=8,bg="CYAN")
@@ -46,18 +28,98 @@ flb.pack(side=TOP)
 lblinfo=Label(Tops,font=('arial',45,'bold'),text="EMPLOYEE ATTENDANCE SYSTEM ",bd=10,fg="GREEN")
 lblinfo.grid(row=0,column=0)
 
+####################################### Root Exit function ####################################
+
 def exit():
   exit=tkinter.messagebox.askyesno("Employee system","Do you want to exit the system")
   if exit>0:
     root.destroy()
     return
+ 
+def insert_fun(userName,Name,mobile_No,Mail_id,Passwd,cityy,photo):
+   def convertToBinaryData(filename):
+      # Convert digital data to binary format
+      with open(filename, 'rb') as file:
+         binaryData = file.read()
+      return binaryData
+   flag=1
+   try:
+      connection = mysql.connector.connect(host='localhost',
+                                             database='employee',
+                                             user='root',
+                                             password='S@qlein05')
 
-personList=[]
-personList_p=[]
+      cursor = connection.cursor()
+      
+      sql= """select username from employee where username like %s"""
+      tup_p=(userName,)
+      cursor.execute(sql,tup_p)
+      for i in cursor:
+         flag=0
+     
+      if(flag==1):
+         sql_insert_blob_query = """ INSERT INTO employee
+                          VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
+         empPicture = convertToBinaryData(photo)
+         unix=time.time()
+         date=str(datetime.datetime.fromtimestamp(unix).strftime("%D %H:%M:%S "))
+         insert_blob_tuple = (userName,Name,mobile_No,Mail_id,Passwd,cityy,empPicture,date)
+         result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+         connection.commit()
+         #lblName_P=Label(fla,text="saved , press done",font=('arial',14,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+      else:
+         lblName_P=Label(fla,text="username is already taken\n try again ",font=('arial',14,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+   except mysql.connector.Error as error:
+      print("Failed inserting user data into MySQL table {}".format(error))
 
+   finally:
+      if (connection.is_connected()):
+         cursor.close()
+         connection.commit()
+         connection.close()
+   return flag
+
+def punch_fun(UserName , UserPasswword):
+   flag =0
+   def convertToBinaryData(filename):
+      # Convert digital data to binary format
+      with open(filename, 'rb') as file:
+         binaryData = file.read()
+      return binaryData
+
+   try:
+      connection = mysql.connector.connect(host='localhost',
+                                             database='employee',
+                                             user='root',
+                                             password='S@qlein05')
+   
+      cursor = connection.cursor()
+      unix=time.time()
+      date=str(datetime.datetime.fromtimestamp(unix).strftime("%D %H:%M:%S "))
+      sql= """select username from employee where username LIKE %s and UserPassword like %s """
+      tup=(UserName,UserPasswword,)
+      cursor.execute(sql,tup)
+      empPicture = convertToBinaryData("C:/Users/WIN 10/vs code program/saved_img.jpg")
+      tupp=(UserName,date,empPicture)
+      sql_p="""insert into punch values (%s,%s,%s)"""  
+      for i in cursor:
+         flag=1
+         capture.PHOTO_T()
+         cursor.execute(sql_p,tupp)
+   except mysql.connector.Error as error:
+      print("Failed inserting punching data into MySQL table {}".format(error))  
+    
+   finally:
+      if (connection.is_connected()):
+         cursor.close()
+         connection.commit()
+         connection.close()
+         print("MySQL connection is closed") 
+   return flag
+   
+##################################### New Employee entry function #############################
 def enterinfo():
-   reset() 
-       
+   e.reset(user_name,name,passwd,mob_n,mail,city)      
    lblName=Label(fla,text="USER NAME",font=('arial',16,'bold'),bd=20,fg="BLACK",bg="WHITE").grid(row=0,column=0)
    lblName=Label(fla,text="Name",font=('arial',16,'bold'),bd=20,fg="BLACK",bg="WHITE").grid(row=0,column=2)
    lblAddress=Label(fla,text="MOBILE NO",font=('arial',16,'bold'),bd=20,fg="BLACK",bg="WHITE").grid(row=1,column=0) 
@@ -65,7 +127,7 @@ def enterinfo():
    lblNINumber=Label(fla,text="PASSWORD",font=('arial',16,'bold'),bd=20,fg="BLACK",bg="WHITE").grid(row=2,column=0)
    lblNINumber=Label(fla,text="City",font=('arial',16,'bold'),bd=20,fg="BLACK",bg="WHITE").grid(row=2,column=2)
 
-   etxhoursworked=Entry(fla,textvariable=passwd,font=('arial',16,'bold'),bd=16,width=22,justify='left')
+   etxhoursworked=Entry(fla,textvariable=passwd,font=('arial',16,'bold'),bd=16,width=22,justify='left',show='*')
    etxhoursworked.grid(row=2,column=1)
 
    
@@ -83,8 +145,9 @@ def enterinfo():
 
    etxhoursworked=Entry(fla,textvariable=city,font=('arial',16,'bold'),bd=16,width=22,justify='left')
    etxhoursworked.grid(row=2,column=3)
+
        
-   def update_info():
+   def update_info():                                                       # update function of following save button
       txtpayslip.delete("1.0",END)
       txtpayslip.insert(END,"\tEMPLOYEE INFORMATION\n\n")
       txtpayslip.insert(END,"USER NAME :\t"+user_name.get()+"\n\n")
@@ -93,19 +156,37 @@ def enterinfo():
       txtpayslip.insert(END,"password :\t\t"+passwd.get()+"\n\n")
       txtpayslip.insert(END,"mail id :\t"+mail.get()+"\n\n")
       txtpayslip.insert(END,"City :\t"+city.get()+"\n\n")
-      personList.append(person(user_name.get(),name.get(),mob_n.get(),passwd.get(),mail.get(),city.get()))
-      email_g(user_name.get(),name.get(),passwd.get(),mail.get(),mob_n.get(),city.get())
-   btnsave=Button(fla,text='SAVE',padx=25,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=update_info,fg="BLACK",bg="WHITE").grid(row=3,column=3)   
+      user_n=user_name.get()
+      namee=name.get()
+      mob=mob_n.get()
+      maild=mail.get()
+      passs=passwd.get()   
+      cityy=city.get()
+      #capture.PHOTO_T()
+      flag = insert_fun(user_n,namee,mob,maild,passs,cityy,"C:/Users/WIN 10/vs code program/saved_img.jpg")
+      lblName_Pp=Label(fla,text="Please wait..while processing",font=('arial',14,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+      #e.email_entry(maild)
+      lblName_P=Label(fla,text="Entry is saved , press done",font=('arial',14,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+      def doned_entry():
+         root.destroy()
+      if(flag==1):
+         capture.PHOTO_T()
+         btn_conp=Button(fla,text='Done',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=doned_entry,fg="red",bg="WHITE").grid(row=3,column=2)
+      else:
+         btn_conp=Button(fla,text='Username is already taken',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=doned_entry,fg="red",bg="WHITE").grid(row=3,column=2)          
+   btnsave=Button(fla,text='SAVE',padx=25,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=update_info,fg="BLACK",bg="WHITE").grid(row=3,column=3)        # save button of root entry interfcae
 
+
+############################################################   punching function #########################
 def punch():
-   for person in personList:
-      print(person.cityy)
+   ch_user=StringVar()
+   ch_pass=StringVar()
    root_t=Tk()
    root_t.title("WELCOME TO PUNCHING PROCESS")
-   root_t.geometry('1350x650+0+0')
+   root_t.geometry('1550x850+0+0')
    root_t.configure(background="CYAN")
  
-   def exit_p():
+   def exit_p():                   # exit function of punching interface , it is nested function
       exit_p=tkinter.messagebox.askyesno("Employee system","Do you want to exit the system")
       if exit_p>0:
          root_t.destroy()
@@ -113,115 +194,53 @@ def punch():
 
    f3=Frame(root_t,width=600,height=600,bd=8,bg="WHITE")
    f3.pack(side=LEFT)
-   f4=Frame(root_t,width=300,height=700,bd=8,bg="WHITE")
-   f4.pack(side=RIGHT)
    
    flc=Frame(f3,width=600,height=200,bd=8,bg="CYAN")
    flc.pack(side=TOP)  
- 
-   btn_exit=Button(flc,text='Exit System',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=exit_p,fg="red",bg="WHITE").grid(row=4,column=5)  
-   
-   lblName_p=Label(flc,text="ENTER USER NAME",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=0,column=0)
-   global e_usr
-   e_usr=Entry(flc,textvariable=ch_user,font=('arial',16,'bold'),bd=16,width=30,justify='left')
+
+   btn_exit=Button(flc,text='Exit',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=exit_p,fg="red",bg="WHITE").grid(row=4,column=5) ##Exit Button of punching interface 
+   lblName_p=Label(flc,text="Username",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=0,column=0) # enter user name label 
+   e_usr=Entry(flc,textvariable=ch_user,font=('arial',16,'bold'),bd=16,width=30,justify='left')               # entry field of user name of punching 
    e_usr.grid(row=0,column=1)
-        
-   txtpayslip.insert(END," empty :\t"+ch_user.get()+"\n\n")
-   personList_p.append(personp(ch_user.get()))
-   print(ch_user.get())
-   
+   lblName_P=Label(flc,text="Password",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=1,column=0) #enter password label of punching field 
+   e_pass=Entry(flc,textvariable=ch_pass,font=('arial',16,'bold'),bd=16,width=30,justify='left',show='*')                          # entry field of password   
+   e_pass.grid(row=1,column=1)
+
    def con():
-      lblName_P=Label(flc,text="Enter your password",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=1,column=0)
-      e_pass=Entry(flc,textvariable=ch_pass,font=('arial',16,'bold'),bd=16,width=30,justify='left')
-      e_pass.grid(row=1,column=1)
-      btn_conp=Button(flc,text='DONE',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=doned,fg="red",bg="WHITE").grid(row=3,column=2) 
+      flag=punch_fun(e_usr.get(),e_pass.get())
+      if(flag==1):
+         lblName_P=Label(flc,text="Your Punching is Successful ,Press Done to Save",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+         btn_conp=Button(flc,text='Done',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=doned,fg="red",bg="WHITE").grid(row=3,column=2)   # after entering password done button is here
+      else:
+         lblName_P=Label(flc,text="Invalid Credintials , Please try again",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
+
+
    def doned():
-      lblName_P=Label(flc,text="YOUR PUNCHING IS SUCCESFULL",font=('arial',16,'bold'),bd=20,fg="red",bg="CYAN").grid(row=6,column=0)
-      email_gf()
-                
-   btn_conp=Button(flc,text='NEXT',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=con,fg="red",bg="WHITE").grid(row=4,column=2)
+      root.destroy()
+      e.email_punching(e_usr.get())
+      root_t.destroy()
+   btn_conp=Button(flc,text='Submit',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=con,fg="red",bg="WHITE").grid(row=4,column=2)    # next button to process after entering username 
 
 
-
-
-
-def email_g(user_name,ename,pass_g,mail_g,mob_g,city_g):
-   print("in process mail")
-   global mail_pp,ename_p
-   mail_pp=mail_g
-   ename_p=ename   
-   s = smtplib.SMTP('smtp.gmail.com', 587) 
-   s.starttls()
-   s.login("Employee.Attenddance.System@gmail.com", "empatt321") 
-   message = "Subject:New Entry recorded \n\t\nDear " + str(ename) + " \n\n\tyour details are succesfully recorded\n\nyour User name is : " + str(user_name) +"\nyour password is: "+str(pass_g)+ "\nyour mobile number is : "+ str(mob_g) + "\nyour city is : "+ city_g + "\n\nWe are glad to see you working with us \n\n\t\t[ HAVE A GOOD DAY ]"+"\n\n\n\nManaged by:-\nSaqlein shaikh\nMahesh Sawant\nShivansh Thakur"
-   s.sendmail("Employee.Attenddance.System@gmail.com", mail_g, message)
-   print("\nsuccesfully sent") 
-   s.quit()
-
-def email_gf():
-   now_p = datetime.now()
-   global dt_stringg 
-   dt_stringg = now_p.strftime("%d/%m/%Y  \t%H:%M:%S")
-   print("in process mail")   
-   s = smtplib.SMTP('smtp.gmail.com', 587) 
-   s.starttls()
-   print("punching mail is in process") 
-   s.login("Employee.Attenddance.System@gmail.com", "empatt321") 
-   message = "subject:punching confirm \n\nDear " + ename_p + " \n\n\tyour punching is succesfully recorded of date \n\n"+ dt_stringg +"\n\n\t\t[ HAVE A GOOD DAY ]"+"\n\n\n\nManaged by:-\nSaqlein shaikh\nMahesh Sawant\nShivansh Thakur"
-   s.sendmail("Employee.Attenddance.System@gmail.com", mail_pp, message)
-   print("\nsuccesfully sent") 
-   s.quit()
-
-
-def reset():
-  user_name.set("")
-  name.set("")
-  passwd.set("")
-  mob_n.set("")
-  mail.set("")
-  city.set("") 
-
-######variables###
+#####################################################  Intilizing variables ###########################################
 user_name=StringVar()
 name=StringVar()
 passwd=StringVar()
 mob_n=StringVar()
 mail=StringVar()
 city=StringVar()
-ch_user=StringVar()
-ch_pass=StringVar()
 
-#######date########
-now = datetime.now()
-global dt_string 
-dt_string = now.strftime("%d/%m/%Y  \t%H:%M:%S")
+#########################Display process##########################
+  
+txtpayslip=Text(f2,height=22,width=40,bd=16,font=('arial',13,'bold'),fg="green",bg="powder blue")
+txtpayslip.grid(row=1,column=0)
 
-#=============================== buttons ===============================================================
-'''btnsalary=Button(flb,text='NEW ENTRY',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,fg="red",bg="powder blue",command=enterinfo,grid(row=0,column=0)'''
+#===============================root (New entry ) main buttons ================================s===============================
 
-btnreset=Button(flb,text='NEW ENTRY',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=enterinfo,fg="red",bg="powder blue").grid(row=0,column=0)
+btnreset=Button(flb,text='NEW ENTRY',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=enterinfo,fg="red",bg="powder blue").grid(row=0,column=2)  
 
 btnpayslip=Button(flb,text='PUNCHING',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=punch,fg="red",bg="powder blue").grid(row=0,column=4)
 
 btnexit=Button(flb,text='Exit System',padx=16,pady=16,bd=8,font=('arial',16,'bold'),width=14,command=exit,fg="red",bg="powder blue").grid(row=0,column=6)
-#########################Display process##########################
-  
-txtpayslip=Text(f2,height=22,width=34,bd=16,font=('arial',13,'bold'),fg="green",bg="powder blue")
-txtpayslip.grid(row=1,column=0)
-  
+
 root.mainloop()
-# create 2D array of table given above   
-# dataframe created with 
-# the above data array 
-df = pd.DataFrame(data, columns = [ 'city', ] ) 
-
-size=[8895,753]
-sentiment=['nashik','pune']
-colors=['blue','pink']
-plt.pie(size,labels=sentiment,colors=colors,startangle=180,autopct='%.1f%%')
-plt.title('% city analysis of employee')  
-# create histogram for numeric data  
-# show plot 
-plt.show() 
-
-
